85f11b85abdff8ba01a387a21f6d8d81703f4168
CKMolinga <ck.molinga@gmail.com>
2022-07-08 16:46:19 +0200

    Feature filtering and searching

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,18 +6,27 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // If we have any filters defined, call tasksService, getTasksWithFilters
+    // otherwise, just get all tasks
+
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
